b307b4a3ad45174fa0f343695457ef6b56a8d4e0
Huy <sonogami.rin@gmail.com>
2020-08-21 14:23:20 +0700

    my commit

---
+++ TuanDataStructures/src/Utils.java
@@ -0,0 +1,10 @@
+public class Utils {
+    public static void main(String[] args) {
+        System.out.println("/*public static void main(){}");
+    }
+    public String abc = " public static void abc()";
+    private void main133()
+    {
+        System.out.println("public static void main(){}*/");
+    }
+}
